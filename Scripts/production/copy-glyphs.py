8a294929e07c2f93e8c969df3b8774d3b504f9b1
gferreira <gustavo@hipertipo.com>
2024-01-19 15:55:00 -0300

    making glyphs compatible across all sources

--- Scripts/production/copy-glyphs.py
+++ Scripts/production/copy-glyphs.py
@@ -1,49 +1,90 @@
-import glob
+# menuTitle: copy default glyphs to other sources
+
+import os, glob
 from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstructionBuilder
 from hTools3.modules.accents import buildAccentedGlyphs
 
-sourceFont   = '/Users/gferreira/hipertipo/fonts/amstelvar-avar2/Source/Parametric-avar2/Roman/AmstelvarA2-Roman_wght400.ufo'
-targetFolder = '/Users/gferreira/hipertipo/fonts/amstelvar-avar2/Source/Parametric-avar2/Roman'
+familyName    = 'AmstelvarA2'
+subFamilyName = 'Roman'
+sourceName    = 'wght400'
+baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
+sourcePath    = os.path.join(sourcesFolder, f'{familyName}-{subFamilyName}_{sourceName}.ufo')
 
-targetStyles = [
-    # 'XOPQ20',
-    # 'XOPQ265',
-    # 'XTRA234',
-    # 'XTRA550',
-    # 'XTSP-100',
-    # 'XTSP100',
-    # 'YOPQ14',
-    # 'YOPQ132',
-    'YTAS617',
-    'YTAS1000',
-    'YTDE-138',
-    'YTDE-500',
-    'YTFI447',
-    'YTFI1022',
-    # 'YTLC430',
-    # 'YTLC581',
-    # 'YTUC500',
-    # 'YTUC1000',
-    # 'GRAD-300',
-    # 'GRAD500',
+assert os.path.exists(sourcePath)
+
+glyphNames = [
+    'yi-dieresiscomb',
+    'Gamma',
+    'ghe',
 ]
 
-glyphNames = ['E', 'Y']
+targetStyles = [
+    # 'XOPQ4',
+    # 'XOPQ310',
+    # 'XSHF0',
+    # 'XSHF179',
+    # 'XSHL0',
+    # 'XSHL124',
+    # 'XSHU0',
+    # 'XSHU154',
+    # 'XSVF0',
+    # 'XSVF131',
+    # 'XSVL0',
+    # 'XSVL98',
+    # 'XSVU0',
+    # 'XSVU130',
+    # 'XTRA208',
+    # 'XTRA508',
+    # 'XTSP-100',
+    # 'XTSP100',
+    # 'XTTW0',
+    # 'XTTW30',
+    # 'YOPQ4',
+    # 'YOPQ311',
+    # 'YSHF0',
+    # 'YSHF147',
+    # 'YSHL0',
+    # 'YSHL140',
+    # 'YSHU0',
+    # 'YSHU150',
+    # 'YSVF84',
+    # 'YSVF251',
+    # 'YSVL146',
+    # 'YSVL251',
+    'YSVU0',
+    'YSVU165',
+    'YTAS665',
+    'YTAS875',
+    'YTDE-100',
+    'YTDE-310',
+    'YTFI281',
+    'YTFI896',
+    'YTLC436',
+    'YTLC594',
+    'YTOS0',
+    'YTOS25',
+    'YTTL0',
+    'YTTL104',
+    'YTUC541',
+    'YTUC875',
+]
 
-targetFonts = glob.glob(f'{targetFolder}/*.ufo')
-targetFonts.remove(sourceFont)
+sourceFont = OpenFont(sourcePath, showInterface=False)
 
-font = OpenFont(sourceFont, showInterface=False)
-
-for dstPath in targetFonts:
+for styleName in targetStyles:
+    dstPath = os.path.join(sourcesFolder, f'{familyName}-{subFamilyName}_{styleName}.ufo')
+    if not os.path.exists(dstPath):
+        print(f'target font {dstPath} does not exist.\n')
     dstFont = OpenFont(dstPath, showInterface=False)
 
     print(f'copying glyphs to {dstPath}...')
     for glyphName in glyphNames:
         print(f'\tcopying {glyphName}...')
-        dstFont.insertGlyph(font[glyphName], name=glyphName)
+        dstFont.insertGlyph(sourceFont[glyphName], name=glyphName)
 
+    print(f'\tsaving font...')
     dstFont.save()
-    dstFont.close()
+    # dstFont.close()
     print()
 
