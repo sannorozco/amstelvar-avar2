1f917c172ba71b77c80ce6d92ae47aa40b78a831
gferreira <gustavo@hipertipo.com>
2024-01-23 10:03:40 -0300

    extracting fresh measurements from Amstelvar1 extrema

--- Scripts/production/extract-measurements.py
+++ Scripts/production/extract-measurements.py
@@ -1,44 +1,68 @@
-# menuTitle: extract measurements
+# menuTitle: extract parametric blends from Amstelvar1 extrema
 
 import os, glob, json
 from variableValues.measurements import FontMeasurements
 
-familyName       = 'Amstelvar2'
-subFamilyName    = ['Roman', 'Italic'][0]
-baseFolder       = os.path.dirname(os.getcwd())
-sourcesFolder    = os.path.join(baseFolder, 'TechAlpha', subFamilyName)
-extremaFolder    = os.path.join(sourcesFolder, 'extrema')
-measurementsPath = os.path.join(sourcesFolder, 'measurements.json')
+subfamilyName    = ['Roman', 'Italic'][0]
+baseFolder1      = '/Users/gferreira/hipertipo/fonts/amstelvar' # path to Amstelvar1 sources for measuring - see http://github.com/gferreira/amstelvar
+sourcesFolder1   = os.path.join(baseFolder1, subfamilyName)
+measurementsPath = os.path.join(sourcesFolder1, 'measurements.json')
+baseFolder2      = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder2   = os.path.join(baseFolder2, 'Sources', subfamilyName)
+blendsPath       = os.path.join(sourcesFolder2, 'blends_full.json')
+parametricAxes   = 'XOPQ XTRA YOPQ YTUC YTLC YTAS YTDE YTFI XSHU YSHU XSVU YSVU XSHL YSHL XSVL YSVL XSHF YSHF XSVF YSVF XTTW YTTL YTOS XUCS'.split()
+
+# define blended axes
+
+blendedAxes = {
+    "opsz": {
+      "name"    : "Optical size",
+      "default" : 14,
+      "min"     : 8,
+      "max"     : 144,
+    },
+    "wght": {
+      "name"    : "Weight",
+      "default" : 400,
+      "min"     : 200,
+      "max"     : 800,
+    },
+    "wdth": {
+      "name"    : "Width",
+      "default" : 100,
+      "min"     : 85,
+      "max"     : 125,
+    }
+}
+
+# extract measurements from Amstelvar1
 
 assert os.path.exists(measurementsPath)
+ufos = glob.glob(f'{sourcesFolder1}/*.ufo')
 
-ufos = [
-    'Amstelvar-Roman_opsz8.ufo', 
-    'Amstelvar-Roman_opsz144.ufo',
-]
-
-measurements = {}
-
-for ufo in ufos:
-    ufoPath = os.path.join(extremaFolder, ufo)
+blendedSources = {}
+for ufoPath in sorted(ufos):
     assert os.path.exists(ufoPath)
-
     fontName = os.path.splitext(os.path.split(ufoPath)[-1])[0]
-    styleName = fontName.split('_')[-1]
-
+    styleName = '_'.join(fontName.split('_')[1:])
+    # don't include the default
+    if styleName == 'wght400':
+        continue
+    # ignore GRAD sources
+    if 'GRAD' in styleName:
+        continue
     f = OpenFont(ufoPath, showInterface=False)
-
     M = FontMeasurements()
     M.read(measurementsPath)
     M.measure(f)
+    blendedSources[styleName] = { k: v for k, v in M.values.items() if k in parametricAxes }
 
-    measurements[styleName] = M.values
+# save measurements to JSON blends file
 
-print(measurements)
+blendsDict = {
+    'axes'    : blendedAxes,
+    'sources' : blendedSources,
+}
 
-# # save blends in JSON format
-
-# jsonPath = os.path.join(ufosFolder, 'blends.json')
-
-# with open(jsonPath, 'w', encoding='utf-8') as f:
-#     json.dump(measurements, f, indent=2)
+with open(blendsPath, 'w', encoding='utf-8') as f:
+    json.dump(blendsDict, f, indent=2)
