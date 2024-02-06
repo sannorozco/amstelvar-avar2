a9b16f88a9df7ad50d88c01d77cb303c83855647
gferreira <gustavo@hipertipo.com>
2024-02-06 09:58:09 -0300

    restoring Roman measurements file (overwritten by Italic), copying script to mark components, fixing vertical serifs (WIP)

---
+++ Scripts/production/mark-components.py
@@ -0,0 +1,38 @@
+# menuTitle: mark glyphs with components
+
+'''
+red    : nested components
+blue   : multiple components
+yellow : one component only
+
+'''
+
+alpha = 0.8
+
+f = CurrentFont()
+
+def getNestingLevels(g, levels=0):
+    if g.components:
+        levels += 1
+        for c in g.components:
+            if c.baseGlyph not in f:
+                print(f'ERROR in "{g.name}": glyph {c.baseGlyph} not in font')
+                continue
+            baseGlyph = f[c.baseGlyph]
+            levels = getNestingLevels(baseGlyph, levels)
+    return levels
+
+for g in f:
+    g.markColor = None
+    if not(g.components):
+        continue
+    if len(g.components) == 1:
+        g.markColor = 1, 1, 0, alpha
+    else:
+        levels = getNestingLevels(g)
+        if levels > 1:
+            g.markColor = 1, 0, 0, alpha
+        else:
+            g.markColor = 0, 0.5, 1, alpha
+
+f.changed()
