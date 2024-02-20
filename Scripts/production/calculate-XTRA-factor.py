4d6fda131fa8a3db7033c1a966925d46e0133aa8
gferreira <gustavo@hipertipo.com>
2024-02-20 11:17:56 -0300

    working on cyrillic XTRA min/max, updating measurements, improving XTRA calculator

---
+++ Scripts/production/calculate-XTRA-factor.py
@@ -0,0 +1,43 @@
+import os
+from variableValues.measurements import FontMeasurements, Measurement, permille
+from variableValues.linkPoints import readMeasurements
+
+familyName       = 'AmstelvarA2'
+subFamilyName    = ['Roman', 'Italic'][0]
+baseFolder       = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder    = os.path.join(baseFolder, 'Sources', subFamilyName)
+measurementsPath = os.path.join(sourcesFolder, 'measurements.json')
+defaultPath      = os.path.join(sourcesFolder, f'{familyName}-{subFamilyName}_wght400.ufo')
+measurementsDict = readMeasurements(measurementsPath)
+
+def makeMeasurementArgs(glyphName, i=0):
+    args = [[v['name'], v['direction']] + k.split() for k, v in measurementsDict['glyphs'][glyphName].items() if v['name'] == 'XTUC'][i]
+    args.insert(2, glyphName)
+    args.insert(4, glyphName)
+    return args
+
+g = CurrentGlyph()
+
+assert g is not None
+
+i = 0
+
+srcFont  = OpenFont(defaultPath, showInterface=False)
+dstFont  = CurrentFont()
+srcGlyph = 'H'
+dstGlyph = g.name
+
+print(dstGlyph)
+
+M1 = Measurement(*makeMeasurementArgs(srcGlyph))
+srcReference = M1.measure(srcFont)
+dstReference = M1.measure(dstFont)
+
+M2 = Measurement(*makeMeasurementArgs(dstGlyph, i))
+srcValue = M2.measure(srcFont)
+dstValue = srcValue * dstReference / srcReference
+
+print(f'{srcValue} -> {round(dstValue)}')
+
+dstFont[dstGlyph].rightMargin = srcFont[dstGlyph].rightMargin
+
--- Scripts/production/calculate-factor-XTRA.py
+++
@@ -1,5 +0,0 @@
-srcReference = 800
-dstReference = 1017
-srcValue     = 233
-dstValue     = srcValue * dstReference/srcReference
-print(round(dstValue))
