b3363daa1efb63ee004df84facd5cb4ccdba6ffa
gferreira <gustavo@hipertipo.com>
2024-04-12 19:45:46 -0300

    updating blends with WDSP values, rebuilding designspace and avar2 font

--- Tools/build.py
+++ Tools/build.py
@@ -565,10 +565,10 @@ if __name__ == '__main__':
     # D1.save()
     # D1.buildVariableFont()
 
-    # D2 = AmstelvarA2DesignSpaceBuilder_avar2()
-    # D2.build()
-    # D2.save()
-    # D2.buildVariableFont()
+    D2 = AmstelvarA2DesignSpaceBuilder_avar2()
+    D2.build()
+    D2.save()
+    D2.buildVariableFont()
 
     # D3 = AmstelvarA2DesignSpaceBuilder_avar2_fences()
     # D3.build()
--- Tools/production/validate-locations.py
+++ Tools/production/validate-locations.py
@@ -5,13 +5,22 @@ folder = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(folder, 'Sources', 'Roman')
 designspacePath = os.path.join(sourcesFolder, 'AmstelvarA2-Roman.designspace')
 
+_sources   = True
 _instances = True
-_sources   = False
 
 assert os.path.exists(designspacePath)
 
 doc = DesignSpaceDocument()
 doc.read(designspacePath)
+
+# validate sources
+if _sources:
+    locations = []
+    for src in doc.sources:
+        if src not in locations:
+            locations.append(src.location)
+        else:
+            print(src.name, src.location)
 
 # validate instances
 if _instances:
@@ -23,13 +32,4 @@ if _instances:
             if not axis.minimum <= value <= axis.maximum:
                 print(f"!! {axisName} {value} ({axis.minimum} {axis.maximum}) {'-' if value < axis.minimum else '+' if value > axis.maximum else ''} ")
         print()
-
-# validate sources
-if _sources:
-    locations = []
-    for src in doc.sources:
-        if src not in locations:
-            locations.append(src.location)
-        else:
-            print(src.name, src.location)
 
