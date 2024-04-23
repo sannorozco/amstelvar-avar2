51e3c4652c6f8c28d50ae9d020fa0d41820c0841
gferreira <gustavo@hipertipo.com>
2024-04-23 13:40:11 -0300

    removing dieresistonoscomb.case dieresistonos.case from all Roman & Italic sources (thanks Irene)

---
+++ Tools/production/remove-glyphs.py
@@ -0,0 +1,22 @@
+# menuTitle: remove glyphs from all sources
+
+import os, glob
+
+familyName    = 'AmstelvarA2'
+subFamilyName = ['Roman', 'Italic'][1]
+baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
+ufoPaths      = glob.glob(f'{sourcesFolder}/*.ufo')
+glyphNames    = 'dieresistonoscomb.case dieresistonos.case'.split()
+
+for ufoPath in ufoPaths:
+    f = OpenFont(ufoPath, showInterface=False)
+    print(f'removing glyphs in {ufoPath}...')
+    for glyphName in glyphNames:
+        if glyphName not in f:
+            continue
+        print(f'\tremoving {glyphName}...')
+        del f[glyphName]
+    f.save()
+    f.close()
+    print()
