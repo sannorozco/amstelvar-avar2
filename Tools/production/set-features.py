f94d8c8c6e190436e6140d38b6babf4143295cc8
gferreira <gustavo@hipertipo.com>
2024-04-25 17:25:10 -0300

    [Italic] more work on XOPQ, adding dummy blends file, building test avar2 font

--- Tools/production/set-features.py
+++ Tools/production/set-features.py
@@ -1,11 +1,11 @@
+# menuTitle: include external features in parametric sources
+
 import os, glob
 
 familyName    = 'AmstelvarA2'
-subFamilyName = ['Roman', 'Italic'][0]
-baseFolder    = os.path.dirname(os.getcwd())
-sourcesFolder = os.path.join(baseFolder, 'Parametric-avar2', subFamilyName)
-
-# set feature in parametric sources
+subFamilyName = ['Roman', 'Italic'][1]
+baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 
 assert os.path.exists(sourcesFolder)
 
