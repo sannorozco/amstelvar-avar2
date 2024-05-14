68d2d15217ba0e330064cea1ea1c10cc3cbfe908
gferreira <gustavo@hipertipo.com>
2024-05-14 08:03:49 -0300

    [Italic] fixing compatibility issues, rebuilding avar2 font

--- Tools/build.py
+++ Tools/build.py
@@ -650,9 +650,9 @@ if __name__ == '__main__':
     # D0.build()
     # D0.save()
 
-    D = AmstelvarA2DesignSpaceBuilder()
-    D.build(blends=True, instances=True)
-    D.save()
+    # D = AmstelvarA2DesignSpaceBuilder()
+    # D.build(blends=True, instances=True)
+    # D.save()
     # D.buildInstances()
 
     # D1 = AmstelvarA2DesignSpaceBuilder_avar1()
@@ -663,7 +663,7 @@ if __name__ == '__main__':
     D2 = AmstelvarA2DesignSpaceBuilder_avar2()
     D2.build()
     D2.save()
-    # D2.buildVariableFont(subset=None, setVersionInfo=True, debug=False)
+    D2.buildVariableFont(subset=None, setVersionInfo=True, debug=False)
 
     # D3 = AmstelvarA2DesignSpaceBuilder_avar2_fences()
     # D3.build()
