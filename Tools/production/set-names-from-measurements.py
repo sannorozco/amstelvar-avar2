aa6e8067ae9a8f531aa657e83dc41e2fbf0e42a5
gferreira <gustavo@hipertipo.com>
2024-06-12 07:43:07 -0300

    adding designspace with instances in user locations, rebuilding avar2 fonts (Roman), fixing XTRA range

--- Tools/production/set-names-from-measurements.py
+++ Tools/production/set-names-from-measurements.py
@@ -7,7 +7,7 @@ from collections import Counter
 from variableValues.measurements import FontMeasurements
 
 familyName       = 'AmstelvarA2'
-subFamilyName    = ['Roman', 'Italic'][1]
+subFamilyName    = ['Roman', 'Italic'][0]
 baseFolder       = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder    = os.path.join(baseFolder, 'Sources', subFamilyName)
 measurementsPath = os.path.join(sourcesFolder, 'measurements.json')
