0d9b188ba274358e0c9af36d98f8ae7ad0ee08a2
gferreira <gustavo@hipertipo.com>
2024-08-08 05:18:40 -0300

    removing leftover .rvrn glyphs from Roman sources (Oslash oslash + bars)

--- Tools/production/remove-glyphs.py
+++ Tools/production/remove-glyphs.py
@@ -3,16 +3,17 @@
 import os, glob
 
 familyName    = 'AmstelvarA2'
-subFamilyName = ['Roman', 'Italic'][1]
+subFamilyName = ['Roman', 'Italic'][0]
 baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 ufoPaths      = glob.glob(f'{sourcesFolder}/*.ufo')
 
+glyphNames = 'diagonalbarO.rvrn diagonalbaro.rvrn Oslash.rvrn oslash.rvrn'.split()
 # glyphNames = 'dollar.rvrn cent.rvrn naira.rvrn won.rvrn kip.rvrn peso.rvrn cedi.rvrn colonsign.rvrn dollar.rvrn2 guarani.rvrn'.split()
-glyphNames = 'grave-stack.case acute-stack.case dieresis-stack.case macron-stack.case circumflex-stack.case caron-stack.case breve-stack.case dotaccent-stack.case ring-stack.case tilde-stack.case hungarumlaut-stack.case breveinverted-stack.case dblgrave-stack.case'.split()
+# glyphNames = 'grave-stack.case acute-stack.case dieresis-stack.case macron-stack.case circumflex-stack.case caron-stack.case breve-stack.case dotaccent-stack.case ring-stack.case tilde-stack.case hungarumlaut-stack.case breveinverted-stack.case dblgrave-stack.case'.split()
 
 dstFonts   = []
-preflight  = True
+preflight  = False
 
 for ufoPath in ufoPaths:
     name = os.path.splitext(os.path.split(ufoPath)[-1])[0].split('_')[-1]
