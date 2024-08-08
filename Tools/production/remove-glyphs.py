d7e949e22652e441561e9b0e701e9208b38c6334
gferreira <gustavo@hipertipo.com>
2024-08-08 05:11:41 -0300

    deleting non-combining stack.case accents from all Roman & Italic sources

--- Tools/production/remove-glyphs.py
+++ Tools/production/remove-glyphs.py
@@ -3,14 +3,16 @@
 import os, glob
 
 familyName    = 'AmstelvarA2'
-subFamilyName = ['Roman', 'Italic'][0]
+subFamilyName = ['Roman', 'Italic'][1]
 baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 ufoPaths      = glob.glob(f'{sourcesFolder}/*.ufo')
 
-glyphNames = 'dollar.rvrn cent.rvrn naira.rvrn won.rvrn kip.rvrn peso.rvrn cedi.rvrn colonsign.rvrn dollar.rvrn2 guarani.rvrn'.split()
+# glyphNames = 'dollar.rvrn cent.rvrn naira.rvrn won.rvrn kip.rvrn peso.rvrn cedi.rvrn colonsign.rvrn dollar.rvrn2 guarani.rvrn'.split()
+glyphNames = 'grave-stack.case acute-stack.case dieresis-stack.case macron-stack.case circumflex-stack.case caron-stack.case breve-stack.case dotaccent-stack.case ring-stack.case tilde-stack.case hungarumlaut-stack.case breveinverted-stack.case dblgrave-stack.case'.split()
+
 dstFonts   = []
-preflight  = False
+preflight  = True
 
 for ufoPath in ufoPaths:
     name = os.path.splitext(os.path.split(ufoPath)[-1])[0].split('_')[-1]
