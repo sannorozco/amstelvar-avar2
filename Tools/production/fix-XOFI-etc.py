52706b3c05dfcc1aa65838caba66711fcb1706ea
gferreira <gustavo@hipertipo.com>
2024-11-29 06:11:40 -0300

    adding script to correct **FI and **LC glyph coverage

--- Tools/production/fix-XOFI-etc.py
+++ Tools/production/fix-XOFI-etc.py
@@ -1,5 +1,9 @@
 # menuTitle: fix XOFI XTFI YOFI etc glyph coverage
 
+import os, glob
+from collections import Counter
+from mojo.smartSet import readSmartSets
+
 '''
 1. copy glyphs from lowercase to figures
 2. copy glyphs from default to lowercase
@@ -13,24 +17,85 @@
 
 '''
 
+subfamilyName = ['Roman', 'Italic'][1]
+defaultName   = 'wght400'
+baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder = os.path.join(baseFolder, 'Sources')
+smartSetsPath = os.path.join(sourcesFolder, 'AmstelvarA2.roboFontSets')
+defaultPath   = os.path.join(sourcesFolder, subfamilyName, f'AmstelvarA2-{subfamilyName}_{defaultName}.ufo')
+ufoPaths      = glob.glob(f'{sourcesFolder}/{subfamilyName}/*.ufo')
+
+assert os.path.exists(smartSetsPath)
+assert os.path.exists(defaultPath)
+
+preflight = True
+
 sourcesFigures = {
     'XOFI' : 'XOLC',   # figures : lowercase
     'YOFI' : 'YOLC',
     'XTFI' : 'XTLC',
 }
 
-# get glyph names from smartsets
+smartSets = readSmartSets(smartSetsPath, useAsDefault=False, font=None)
+
+copyGroups = [
+    'fractions',
+    'currency',
+    'math operators',
+    'figures superior',
+]
+
 glyphNames = []
+for smartSet in smartSets:
+    for group in smartSet.groups:
+        if group.name in copyGroups:
+            glyphNames += group.glyphNames
 
-defaultSource = ''
+glyphNames += [
+    'perthousand'
+    'fraction',
+]
 
-# PSEUDO-CODE
-for each figure source axis:
-    for each figure source:
-        get lowercase source;
-        for glyphName in glyphNames:
-            copy lowercase glyph to figure glyph;
-        save and close figure source;
-        for glyphName in glyphNames:
-            copy default glyph to lowercase glyph;
-        save and close lowercase souce;
+defaultSrc = OpenFont(defaultPath, showInterface=False)
+
+for figuresName, lowercaseName in sourcesFigures.items():
+
+    figuresSources = [f for f in ufoPaths if figuresName   in os.path.split(f)[-1]]
+    figuresValues  = {int(os.path.splitext(os.path.split(f)[-1])[0].split('_')[-1][4:]) : f for f in figuresSources }
+
+    lowercaseSources = [f for f in ufoPaths if lowercaseName in os.path.split(f)[-1]]
+    lowercaseValues = {int(os.path.splitext(os.path.split(f)[-1])[0].split('_')[-1][4:]) : f for f in lowercaseSources }
+
+    print(f'fixing {figuresName} and {lowercaseName} sources...')
+    for i, figuresSrcValue in enumerate(sorted(figuresValues.keys())):
+        figuresSrcPath = figuresValues[figuresSrcValue]
+        figuresSrc = OpenFont(figuresSrcPath, showInterface=False)
+
+        lowercaseSrcValue = sorted(lowercaseValues.keys())[i]
+        lowercaseSrcPath = lowercaseValues[lowercaseSrcValue]
+        lowercaseSrc = OpenFont(lowercaseSrcPath, showInterface=False)
+
+        # copy lowercase glyphs to figures source
+        print(f'\tcopying glyphs from {lowercaseName}{lowercaseSrcValue} to {figuresName}{figuresSrcValue}...')
+        if not preflight:
+            for glyphName in glyphNames:
+                if glyphName not in figuresSrc:
+                    print(f'ERROR: {glyphName} not in lowercase font {lowercaseSrcValue}')
+                    continue
+                figuresSrc.insertGlyph(lowercaseSrc[glyphName], name=glyphName)
+            figuresSrc.save()
+            figuresSrc.close()
+
+        # copy default glyphs to lowercase source
+        print(f'\tcopying glyphs from {defaultName} to {lowercaseName}{lowercaseSrcValue}...')
+        if not preflight:
+            for glyphName in glyphNames:
+                if glyphName not in defaultSrc:
+                    print(f'ERROR: {glyphName} not in default font {defaultName}')
+                    continue
+                lowercaseSrc.insertGlyph(defaultSrc[glyphName], name=glyphName)
+            lowercaseSrc.save()
+            lowercaseSrc.close()
+
+        print()
+
