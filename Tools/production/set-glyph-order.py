f85a8119cac185e56c5286c81c77aa4945bce772
gferreira <gustavo@hipertipo.com>
2024-12-05 20:01:24 -0300

    copying Roman glyph order to Italic sources, removing old glyphs in Roman

--- Tools/production/set-glyph-order.py
+++ Tools/production/set-glyph-order.py
@@ -3,7 +3,7 @@
 import os, glob
 
 familyName    = 'AmstelvarA2'
-subFamilyName = ['Roman', 'Italic'][1]
+subFamilyName = ['Roman', 'Italic'][0]
 baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 defaultName   = 'wght400'
@@ -13,8 +13,7 @@ assert os.path.exists(sourcesFolder)
 assert os.path.exists(defaultPath)
 
 srcFont = OpenFont(defaultPath, showInterface=False)
-# glyphOrder = srcFont.glyphOrder
-glyphOrder = srcFont.templateGlyphOrder
+glyphOrder = srcFont.templateGlyphOrder # srcFont.glyphOrder
 
 ufoPaths = glob.glob(f'{sourcesFolder}/*.ufo')
 
