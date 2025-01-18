cff8158a6543ed7d1be96a77cc263e14c0f4aad5
gferreira <gustavo@hipertipo.com>
2025-01-18 09:25:51 -0300

    [Roman] XOFI figures min/max (WIP)

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ fontmake -m Sources/Roman/AmstelvarA2-Roman_avar2.designspace -o variable --outp
 
 echo "Building AmstelvarA2 Italic..."
 fontmake -m Sources/Italic/AmstelvarA2-Italic_avar2.designspace -o variable --output-path Fonts/AmstelvarA2-Italic_avar2.ttf --feature-writer None --keep-direction --verbose ERROR
+
+echo "done!"
