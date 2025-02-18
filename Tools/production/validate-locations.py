5c921f2d24a940b538ccff77748ed25491d3043e
gferreira <gustavo@hipertipo.com>
2025-02-18 07:08:43 -0300

    splitting Roman XUCS into subaxes, updating measurement files, rebuilding designspaces with XUCS XLCS XFIR

--- Tools/production/validate-locations.py
+++ Tools/production/validate-locations.py
@@ -4,10 +4,10 @@ from fontTools.designspaceLib import DesignSpaceDocument
 subFamilyName = ['Roman', 'Italic'][0]
 folder = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(folder, 'Sources', subFamilyName)
-designspacePath = os.path.join(sourcesFolder, f'AmstelvarA2-{subFamilyName}.designspace')
+designspacePath = os.path.join(sourcesFolder, f'AmstelvarA2-{subFamilyName}_avar2.designspace')
 
 _sources   = True
-_instances = True
+_instances = False
 
 assert os.path.exists(designspacePath)
 
@@ -25,12 +25,13 @@ if _sources:
 
 # validate instances
 if _instances:
+    print(f'validating AmstelvarA2 {subFamilyName} instance locations...\n')
     axes = { axis.tag: axis for axis in doc.axes }
     for instance in doc.instances:
         print(instance.name)
         for axisName, value in instance.designLocation.items():
             axis = axes[axisName]
             if not axis.minimum <= value <= axis.maximum:
-                print(f"!! {axisName} {value} ({axis.minimum} {axis.maximum}) {'-' if value < axis.minimum else '+' if value > axis.maximum else ''} ")
+                print(f"\t!! {axisName} {value} ({axis.minimum} {axis.maximum}) {'-' if value < axis.minimum else '+' if value > axis.maximum else ''} ")
         print()
-
+    print('...done.\n')
