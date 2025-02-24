30c825bea6d305b7e235af14e836ef611134cf2f
gferreira <gustavo@hipertipo.com>
2025-02-24 19:05:53 -0300

    rebuilding avar2 fonts

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo "Building variable fonts..."
 
 echo "  Building AmstelvarA2 Roman..."
-fontmake -m Sources/Roman/AmstelvarA2-Roman_avar2.designspace -o variable --output-path Fonts/AmstelvarA2-Roman_avar2.ttf --feature-writer None --keep-direction --verbose DEBUG
+fontmake -m Sources/Roman/AmstelvarA2-Roman_avar2.designspace -o variable --output-path Fonts/AmstelvarA2-Roman_avar2.ttf --feature-writer None --keep-direction --verbose WARNING
 
-# echo "  Building AmstelvarA2 Italic..."
-# fontmake -m Sources/Italic/AmstelvarA2-Italic_avar2.designspace -o variable --output-path Fonts/AmstelvarA2-Italic_avar2.ttf --feature-writer None --keep-direction --verbose WARNING
+echo "  Building AmstelvarA2 Italic..."
+fontmake -m Sources/Italic/AmstelvarA2-Italic_avar2.designspace -o variable --output-path Fonts/AmstelvarA2-Italic_avar2.ttf --feature-writer None --keep-direction --verbose WARNING
 
 echo "done!"
