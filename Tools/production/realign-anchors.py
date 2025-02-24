0d23f161ddf18c1cdcdf8d4fedfdf5206602a576
gferreira <gustavo@hipertipo.com>
2025-02-24 11:46:02 -0300

    fixing sidebearings & anchor positions in Italic XTLC sources, adding realign anchors script, rebuilding avar2 fonts

---
+++ Tools/production/realign-anchors.py
@@ -0,0 +1,38 @@
+# menuTitle: horizontally align anchors based on the default
+
+import os
+from xTools4.modules.fontutils import getGlyphs2
+
+familyName    = 'AmstelvarA2'
+subFamilyName = ['Roman', 'Italic'][1]
+defaultName   = 'wght400'
+baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
+defaultPath   = os.path.join(sourcesFolder, f'{familyName}-{subFamilyName}_{defaultName}.ufo')
+
+anchorNames = [
+    'top',
+    # 'bottom',
+]
+
+defaultFont = OpenFont(defaultPath, showInterface=False)
+
+f = CurrentFont()
+
+glyphNames = getGlyphs2(f)
+
+for glyphName in glyphNames:
+    defaultGlyph = defaultFont[glyphName]
+    glyph = f[glyphName]
+    print(f'realigning anchors in {glyphName}...')
+    glyph.prepareUndo('realigning anchors')
+    for i, a in enumerate(glyph.anchors):
+        if a.name not in anchorNames:
+            continue
+        xPos = defaultGlyph.width / defaultGlyph.anchors[i].x
+        xNew = glyph.width / xPos
+        print(f'\t{a.name}: {a.x} -> {int(xNew)}')
+        a.x = xNew
+    glyph.performUndo()
+    print()
+    
