0c069cb9b9e58c6ef64e33d2d10c6bbf806e51e8
gferreira <gustavo@hipertipo.com>
2025-03-11 19:14:33 -0300

    [Roman] corrections to XUCS XUCR sources, rebuilding avar2 font

--- Tools/production/set-names-from-measurements.py
+++ Tools/production/set-names-from-measurements.py
@@ -37,24 +37,24 @@ for ufo in sorted(allUFOs):
             f.info.familyName = f'{familyName} {subFamilyName}'
 
     if tag in ignoreTags:
-        print(f'getting {tag} value from file name: {os.path.split(ufo)[-1]}...')
+        # print(f'getting {tag} value from file name: {os.path.split(ufo)[-1]}...')
         newValue = newValue1000 = int(os.path.splitext(os.path.split(ufo)[-1])[0].split('_')[-1][4:])    
-        print(f'\t{newValue}')
+        # print(f'\t{newValue}')
     else:
-        print(f'measuring {tag} in {os.path.split(ufo)[-1]}...')
+        # print(f'measuring {tag} in {os.path.split(ufo)[-1]}...')
         m = FontMeasurements()
         m.read(measurementsPath)
         m.measure(f)
         newValue = m.values[tag]
         newValue1000 = round(newValue * 1000 / f.info.unitsPerEm)
-        print(f'\tunits  = {newValue}')
-        print(f'\tpermil = {newValue1000}')
+        # print(f'\tunits  = {newValue}')
+        # print(f'\tpermil = {newValue1000}')
 
     # set style name
     newStyleName = f'{tag}{newValue1000}'
     allNames.append(newStyleName)
     if newStyleName != f.info.styleName:
-        print(f'\tstyle name: {f.info.styleName} --> {newStyleName}' )
+        print(f'updating style name:\n\t{f.info.styleName} --> {newStyleName}\n' )
         if not preflight:
             f.info.styleName = newStyleName
 
@@ -66,11 +66,11 @@ for ufo in sorted(allUFOs):
     f.close()
 
     if ufo != newFilePath:
-        print(f'\tfile name: {os.path.split(ufo)[-1]} --> {newFileName}' )
+        print(f'updating file name:\n\t{os.path.split(ufo)[-1]} --> {newFileName}\n' )
         if not preflight:
             shutil.move(ufo, newFilePath)   
 
-    print()
+    # print()
 
 # find duplicate styles
 duplicates = [k for k, v in Counter(allNames).items() if v > 1]
