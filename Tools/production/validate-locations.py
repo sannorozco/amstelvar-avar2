7255c887f5af4476aa73b6760bcd788c9b4558c2
gferreira <gustavo@hipertipo.com>
2025-03-11 20:58:10 -0300

    [Roman] expanding XTLR range, rebuilding designspace & avar2 font

--- Tools/production/validate-locations.py
+++ Tools/production/validate-locations.py
@@ -29,15 +29,15 @@ if _instances:
     print(f'validating AmstelvarA2 {subFamilyName} instance locations...\n')
     axes = { axis.tag: axis for axis in doc.axes }
     for instance in doc.instances:
-        print(instance.name)
+        # print(instance.name)
         for axisName, value in instance.designLocation.items():
             axis = axes[axisName]
             if not axis.minimum <= value <= axis.maximum:
                 if axisName not in expandAxes:
                     expandAxes[axisName] = []
                 expandAxes[axisName].append(value)
-                print(f"\t!! {axisName} {value} ({axis.minimum} {axis.maximum}) {'-' if value < axis.minimum else '+' if value > axis.maximum else ''} ")
-        print()
+                # print(f"\t!! {axisName} {value} ({axis.minimum} {axis.maximum}) {'-' if value < axis.minimum else '+' if value > axis.maximum else ''} ")
+        # print()
 
     for axisName, values in expandAxes.items():
         print(f'{axisName}:')
