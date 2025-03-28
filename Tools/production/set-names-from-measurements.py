e66d6534de1581c2ac510276441bceede4fc2f9b
gferreira <gustavo@hipertipo.com>
2025-03-27 22:16:21 -0300

    [Roman] fixing compatibility issues, rebuilding avar2 font

--- Tools/production/set-names-from-measurements.py
+++ Tools/production/set-names-from-measurements.py
@@ -11,13 +11,13 @@ from xTools4.modules.measurements import FontMeasurements
 # --------
 
 familyName       = 'AmstelvarA2'
-subFamilyName    = ['Roman', 'Italic'][1]
+subFamilyName    = ['Roman', 'Italic'][0]
 baseFolder       = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder    = os.path.join(baseFolder, 'Sources', subFamilyName)
 measurementsPath = os.path.join(sourcesFolder, 'measurements.json')
 
 ignoreTags = ['wght', 'GRAD'] # 'BARS', 
-preflight  = False
+preflight  = True
 
 # --------
 # do stuff
