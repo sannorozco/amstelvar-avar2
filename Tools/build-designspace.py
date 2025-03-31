ae3b560f3cc479baebe19522d9a5334c6a5f83cf
gferreira <gustavo@hipertipo.com>
2025-03-31 11:00:22 -0300

    rebuilding blending PDF proofs

--- Tools/build-designspace.py
+++ Tools/build-designspace.py
@@ -902,7 +902,7 @@ if __name__ == '__main__':
     D2.build()
     D2.save()
     # D2.buildVariableFont(subset=None, setVersionInfo=True, debug=False)
-    # D2.buildInstancesVariableFont(clear=True, ufo=True)
+    D2.buildInstancesVariableFont(clear=True, ufo=True)
     # D2.printAxes()
 
     # D3 = AmstelvarA2DesignSpaceBuilder_avar2_fences()
