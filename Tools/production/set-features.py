8916321848239688984aa2be6e27528599d3b2be
gferreira <gustavo@hipertipo.com>
2025-04-21 11:02:22 -0300

    adding OpenType features lnum pnum tnum onum to Roman, rebuilding avar2 fonts

--- Tools/production/set-features.py
+++ Tools/production/set-features.py
@@ -3,17 +3,23 @@
 import os, glob
 
 familyName    = 'AmstelvarA2'
-subFamilyName = ['Roman', 'Italic'][1]
+subFamilyName = ['Roman', 'Italic'][0]
 baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
+
+preflight = True
 
 assert os.path.exists(sourcesFolder)
 
 sources = glob.glob(f'{sourcesFolder}/*.ufo')
 
-fea = '' # f'include (features/{familyName}-{subFamilyName}.fea);'
+fea = f'include (features/{familyName}-{subFamilyName}.fea);'
  
 for sourcePath in sources:
     f = OpenFont(sourcePath, showInterface=False)
-    f.features.text = fea
-    f.save()
+    fontName = os.path.splitext(os.path.split(sourcePath)[-1])[0]
+    print(f'setting features in {fontName}...')
+    if not preflight:
+        f.features.text = fea
+        print('saving...\n')
+        f.save()
