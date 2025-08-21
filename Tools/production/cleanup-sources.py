08b9d8648a5bc787fd06a703bc5478e3a62fa3f7
gferreira <gustavo@hipertipo.com>
2025-08-21 07:11:09 -0300

    cleaning up font lib, glyph libs and mark colors in all sources

--- Tools/production/cleanup-sources.py
+++ Tools/production/cleanup-sources.py
@@ -3,11 +3,10 @@
 import os, glob
 
 familyName    = 'AmstelvarA2'
-subFamilyName = ['Roman', 'Italic'][0]
+subFamilyName = ['Roman', 'Italic'][1]
 baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
 
-preflight = False
 ufoPaths  = glob.glob(f'{sourcesFolder}/*.ufo')
 
 ignoreFontLibKeys = [
@@ -18,30 +17,43 @@ ignoreFontLibKeys = [
 fontLibKeys  = []
 glyphLibKeys = []
 
+print('cleaning up sources...')
+
 for ufoPath in ufoPaths:
-    name = os.path.splitext(os.path.split(ufoPath)[-1])[0].split('_')[-1]
+    fileName = os.path.split(ufoPath)[-1]
+    name = os.path.splitext(fileName)[0].split('_')[-1]
+    print(f'\tcleaning up {fileName}...')
     f = OpenFont(ufoPath, showInterface=False)
     # clear font libs
+    print(f'\t\tcleaning up font libs...')
     for k in f.lib.keys():
         if k.startswith('public.') or k in ignoreFontLibKeys:
              continue
         if k not in fontLibKeys:
             fontLibKeys.append(k)
-            del fontLibKeys[k]
+        del f.lib[k]
     # clear glyph libs
+    print(f'\t\tcleaning up glyph libs and mark colors...')
     for g in f:
         for k in g.lib.keys():
             if k not in glyphLibKeys:
                 glyphLibKeys.append(k)
-                del glyphLibKeys[k]
+            del g.lib[k]
         # clear mark color
         g.markColor = None
-
+    # done
+    print()
     f.save()
     f.close()
 
-print(fontLibKeys)
-print()
-print(glyphLibKeys)
-
 print('...done!\n')
+
+print()
+print('deleted font libs:')
+for k in fontLibKeys:
+    print(f'- {k}')
+print()
+print('deleted glyph libs:')
+for k in glyphLibKeys:
+    print(f'- {k}')
+print()
