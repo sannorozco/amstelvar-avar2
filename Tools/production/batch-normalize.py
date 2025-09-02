b1d9e5d0665fde4527cf5f9e3c1846e8baa88c22
gferreira <gustavo@hipertipo.com>
2025-09-02 13:23:49 -0300

    more edits to Roman /V /W, rewriting UFO normalization script

--- Tools/production/batch-normalize.py
+++ Tools/production/batch-normalize.py
@@ -1,30 +1,20 @@
 # menuTitle: batch normalize all sources
 
 import os, glob
-from mojo.UI import getDefault, setDefault, preferencesChanged
+from ufonormalizer import normalizeUFO
 
 familyName    = 'AmstelvarA2'
 subFamilyName = ['Roman', 'Italic'][0]
 baseFolder    = os.path.dirname(os.path.dirname(os.getcwd()))
 sourcesFolder = os.path.join(baseFolder, 'Sources', subFamilyName)
-normalizeMode = 1
-
-normalizeModeOld = getDefault("shouldNormalizeOnSave")
-
-if normalizeModeOld != normalizeMode:
-    setDefault("shouldNormalizeOnSave", normalizeMode)
 
 ufoPaths = glob.glob(f'{sourcesFolder}/*.ufo')
 
 print('batch normalizing UFOs...\n')
 
 for ufoPath in ufoPaths:
-    f = OpenFont(ufoPath, showInterface=False)
     print(f'\tnormalizing {os.path.split(ufoPath)[-1]}...')
-    f.save()
-
-if normalizeModeOld != normalizeMode:
-    setDefault("shouldNormalizeOnSave", normalizeModeOld)
+    normalizeUFO(ufoPath, onlyModified=False, writeModTimes=False)
     
 print()
 print('...done.\n')
